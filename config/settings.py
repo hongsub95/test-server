88734c720494868cd6844c267f6c9df5b7741115
Ubuntu <ubuntu@ip-172-31-44-71.ap-northeast-2.compute.internal>
2023-05-15 07:50:59 +0000

    test commit2

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3!c(tb=r3m&zroc7@@31)rv$7m_@^h#0usm1an!+@l^stk*m*b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
